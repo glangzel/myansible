c3b6ded4a4ae8bdb3ac07df9443b607782ba2254
glangzel <agentp7023@gmail.com>
2026-03-08 15:56:32 +0900

    fix desktop

--- dotfiles/polybar/.config/polybar/launch.sh
+++ dotfiles/polybar/.config/polybar/launch.sh
@@ -8,6 +8,6 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # 3. Polybarを起動（config内のバーの名前が "main" の場合）
 # ログを出力しておくと、SoCのシミュレーションログを確認するのと同じ感覚でデバッグできて便利です
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+polybar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
